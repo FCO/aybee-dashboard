0b1cf6271e40edf285d9f1e8278d6b69bdacc768
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2018-06-24 01:44:44 -0300

    add conditional vars

--- src/GraphQLClient.js
+++ src/GraphQLClient.js
@@ -1,11 +1,20 @@
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient }     from 'apollo-client';
+import { createHttpLink }   from 'apollo-link-http';
+import { setContext }       from 'apollo-link-context';
+import { InMemoryCache }    from 'apollo-cache-inmemory';
 
-const httpLink = createHttpLink({
-    uri: 'http://localhost:5000/graphql',
-});
+let conf;
+
+try {
+    conf = require('./vars.json');
+} catch(e) {
+    conf = {}
+}
+
+const uri = conf.graphqlurl || 'http://localhost:5000/graphql'
+const httpLink = createHttpLink({ uri });
+
+console.log({ uri })
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
